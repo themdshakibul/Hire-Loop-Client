f7ad12637454dba41ab588d9c38d5036f512fae0
themdshakibul <mdshakibul88240@gmail.com>
2026-06-14 23:57:06 +0600

    pdate User Role

--- src/app/dashboard/admin/users/page.jsx
+++ src/app/dashboard/admin/users/page.jsx
@@ -1,14 +1,21 @@
+import AdminUsersTable from "@/Components/Apps/Dashboard/Admin/AdminUsersTable";
 import { getUsersList } from "@/lib/api/users";
 
-const AdminUsersPage = async () => {
+const AdminUserPage = async () => {
   const data = await getUsersList();
-  const users = data.users;
+  const users = data?.users || [];
 
   return (
-    <div>
-      <h2>Admin Users {users.length}</h2>
+    <div className="min-h-screen bg-[#121212] p-8 text-slate-200">
+      <div className="container mx-auto space-y-4">
+        <h2 className="text-xl font-semibold tracking-tight text-slate-100">
+          User Management ({users.length})
+        </h2>
+
+        <AdminUsersTable users={users} />
+      </div>
     </div>
   );
 };
 
-export default AdminUsersPage;
+export default AdminUserPage;
---
+++ src/lib/actions/users.js
@@ -0,0 +1,19 @@
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "../auth";
+import { revalidatePath } from "next/cache";
+
+export const updateUserRole = async (userId, role) => {
+  const data = await auth.api.setRole({
+    body: {
+      userId: userId,
+      role: role,
+    },
+    headers: await headers(),
+  });
+
+  revalidatePath("/dashboard/admin/users");
+
+  return data;
+};
